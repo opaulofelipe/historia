087fb05bd76133b0424317a818c391fb76ffc8e3
Paulo Felipe <80161893+opaulofelipe@users.noreply.github.com>
2026-01-17 17:27:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,35 @@
-const CACHE = "historia-v2";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./dados.json"
+const CACHE_NAME = "historia-pwa-v1";
+const urlsToCache = [
+  ".",
+  "index.html",
+  "style.css",
+  "app.js",
+  "dados.json",
+  "icon-192.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
